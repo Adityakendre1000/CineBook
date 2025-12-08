db51d2101cedf3a40cf1fe202b6040fabc428646
Adityakendre1000 <adityakendre1000@gmail.com>
2025-12-08 19:46:19 +0530

    chore: added toast provider and and updated app.jsx

---
+++ frontend/src/context/ToastContext.jsx
@@ -0,0 +1,28 @@
+import React, { createContext, useContext, useState } from "react";
+import ToastContainer from "../components/ToastContainer";
+
+const ToastContext = createContext();
+
+export const ToastProvider = ({ children }) => {
+  const [toasts, setToasts] = useState([]);
+
+  const addToast = (message, type = "success") => {
+    const id = Date.now();
+    setToasts((prev) => [...prev, { id, message, type }]);
+    
+    setTimeout(() => removeToast(id), 3000);
+  };
+
+  const removeToast = (id) => {
+    setToasts((prev) => prev.filter((toast) => toast.id !== id));
+  };
+
+  return (
+    <ToastContext.Provider value={{ addToast }}>
+      {children}
+      <ToastContainer toasts={toasts} removeToast={removeToast} />
+    </ToastContext.Provider>
+  );
+};
+
+export const useToast = () => useContext(ToastContext);
