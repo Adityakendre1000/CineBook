37542f684d49d0eb714ae2bbd408216d57516679
sumit kumar mahto <sumit.mahto9131@gmail.com>
2025-12-08 21:46:23 +0530

    about us

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,16 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import SignUp from './pages/SignUp';
 import Login from './pages/Login';
 import { ToastProvider } from './context/ToastContext';
+import ErrorPage from './pages/ErrorPage';
 
 const App = () => {
   return (
     <ToastProvider>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
-        <Route path="*" element={<Navigate to="/login" replace />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </ToastProvider>
   );
