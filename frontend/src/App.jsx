e3b9eeb93ffe6ef5f0fca02872ba65e2c0e4f3c5
Humdaan Mirza <mirzahumdaan786@gmail.com>
2025-12-08 20:40:58 +0530

    Finalized Customer View

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import SignUp from './pages/SignUp';
 import Login from './pages/Login';
-
+import CustomerView from './pages/customerView';
 const App = () => (
   <Routes>
+    <Route path="/" element={<Login />} />
     <Route path="/signup" element={<SignUp />} />
     <Route path="/login" element={<Login />} />
-
-    <Route path="*" element={<Navigate to="/login" replace />} />
+    
+    <Route path="/customer" element={<CustomerView />} />
   </Routes>
 );
 
